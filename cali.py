0d78fd376028799e63d0fce999d247b03a29a088
benjimor <benji_moreau@hotmail.fr>
2017-12-18 10:38:54 +0100

    added Lock to protect lattice from concurrent thread access

--- cali.py
+++ cali.py
@@ -7,14 +7,16 @@ from utils.LatticeCypher import generate_cypher_files
 
 def main():
     parser = argparse.ArgumentParser(prog='cali', description='Experiments with lattice.')
-    parser.add_argument('filename', metavar='f', type=str, nargs='+',
+    parser.add_argument('filename', metavar='Filename', type=str, nargs='+',
                         help='json file containing terms')
+    parser.add_argument('nb_threads', metavar='Threads', type=int, nargs='+',
+                        help='the number of maximum threads to handle licenses combination')
     args = parser.parse_args()
     result = json.load(open(args.filename[0]))
     terms = result["terms"]
     level_1 = FirstLeveLGen.generate_minimal_licences_set(terms)
     cali = LicensesLattice(terms, level_1)
-    cali.generate_lattice(100)
+    cali.generate_lattice(args.nb_threads[0])
     generate_cypher_files(cali, len(terms))
 
 
--- lattice/LicensesLattice.py
+++ lattice/LicensesLattice.py
@@ -1,11 +1,15 @@
 from licenses.ConsolidatedLicense import ConsolidatedLicense, hash_label
 from Lattice import Lattice
 from utils.TimerDecorator import fn_timer
-from threading import Thread
+
+from threading import Thread, Lock
 from itertools import combinations, islice
+from sys import exit
 
 label_dict = {}
 licenses_hash_table = {}
+label_dict_lock = Lock()
+licenses_hash_table_lock = Lock()
 
 
 class LicenseCombinor(Thread):
@@ -15,26 +19,36 @@ class LicenseCombinor(Thread):
         self.couples = couples
         self.new_layer = set()
         self.lattice = lattice
+        self.thread_finish = False
+
+    def terminate(self):
+        self.thread_finish = True
 
     def run(self):
         self.new_layer = set()
         for couple in self.couples:
-            cl1 = couple[0]
-            cl2 = couple[1]
-            combined_label = cl1.label | cl2.label
-            if len(combined_label) < self.lattice.height()+1:
-                hashed_label = hash_label(combined_label)
-                if hashed_label in label_dict:
-                    # this licence is already created by combining other licenses
-                    label_dict[hashed_label].parents.append((cl1, cl2))
-                    cl1.childs.append(label_dict[hashed_label])
-                    cl2.childs.append(label_dict[hashed_label])
-                else:
-                    new_license = self.combination_function(cl1, cl2, combined_label)
-                    if self.prune_filter(new_license):
-                        label_dict[hashed_label] = new_license
-                        self.add_in_hash_table(new_license)
-                        self.new_layer.add(new_license)
+            if not self.thread_finish:
+                cl1 = couple[0]
+                cl2 = couple[1]
+                combined_label = cl1.label | cl2.label
+                if len(combined_label) < self.lattice.height()+1:
+                    hashed_label = hash_label(combined_label)
+                    label_dict_lock.acquire()
+                    if hashed_label in label_dict:
+                        label_dict_lock.release()
+                        # this licence is already created by combining other licenses
+                        label_dict[hashed_label].parents.append((cl1, cl2))
+                        cl1.childs.append(label_dict[hashed_label])
+                        cl2.childs.append(label_dict[hashed_label])
+                    else:
+                        new_license = self.combination_function(cl1, cl2, combined_label)
+                        if self.prune_filter(new_license):
+                            label_dict[hashed_label] = new_license
+                            label_dict_lock.release()
+                            self.add_in_hash_table(new_license)
+                            self.new_layer.add(new_license)
+                        else:
+                            label_dict_lock.release()
 
     def combination_function(self, cl1, cl2, label):
         permissions = cl1.permissions & cl2.permissions
@@ -59,10 +73,12 @@ class LicenseCombinor(Thread):
         return True
 
     def add_in_hash_table(self, license):
+        licenses_hash_table_lock.acquire()
         if license.hash in licenses_hash_table:
             licenses_hash_table[license.hash].append(license)
         else:
             licenses_hash_table[license.hash] = [license]
+        licenses_hash_table_lock.release()
 
 
 class LicensesLattice(Lattice):
@@ -93,17 +109,23 @@ class LicensesLattice(Lattice):
             chunk_size = nb_couples / nb_threads
             rest = nb_couples % nb_threads
             threads = []
-            for i in range(0, nb_threads):
-                if i == 0 and rest:
-                    chunk_couples = islice(couples, 0, chunk_size+rest)
-                else:
-                    chunk_couples = islice(couples, 0, chunk_size)
-                threads.append(LicenseCombinor(chunk_couples, self))
-                threads[i].start()
-            new_layer = set()
-            for thread in threads:
-                thread.join()
-                new_layer = new_layer | thread.new_layer
-            self.set += (new_layer,)
+            try:
+                for i in range(0, nb_threads):
+                    if i == 0 and rest:
+                        chunk_couples = islice(couples, 0, chunk_size+rest)
+                    else:
+                        chunk_couples = islice(couples, 0, chunk_size)
+                    threads.append(LicenseCombinor(chunk_couples, self))
+                    threads[i].start()
+                new_layer = set()
+                for thread in threads:
+                    thread.join()
+                    new_layer = new_layer | thread.new_layer
+                self.set += (new_layer,)
+            except KeyboardInterrupt:
+                print "Ctrl-c pressed ..."
+                for thread in threads:
+                    thread.terminate()
+                    exit(1)
             print ("layer {} is ok".format(self.height()-1))
         self.licenses_hash_table = licenses_hash_table
